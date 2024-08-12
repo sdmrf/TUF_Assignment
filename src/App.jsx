b4e2f87060488e0d9fb68a8fb0a07b2aa8f670ef
Syed Maruf Ali <79003834+sdmrf@users.noreply.github.com>
2024-08-12 13:16:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className="App">Edit App.scss to remove this border</div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  // Add Your Github Repository Name Here
+  base: "/React-Template/",
+  plugins: [react()],
+});
